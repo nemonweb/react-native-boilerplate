0b3d13f0267d9d19909149e6444f9ff274f85842
Nikita Tikhomirov <nikitati@gmail.com>
2018-07-13 12:32:34 +0300

    Add precomite

--- storybook/stories/buttons.stories.js
+++ storybook/stories/buttons.stories.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
-import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
+import { storiesOf } from "@storybook/react-native";
+import { action } from "@storybook/addon-actions";
 
-import Button from './Button';
-import CenterView from './CenterView';
+import Button from "./Button";
+import CenterView from "./CenterView";
 
-storiesOf('Button', module)
+storiesOf("Button", module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('with text', () => (
-    <Button onPress={action('clicked-text')}>
+  .add("with text", () => (
+    <Button onPress={action("clicked-text")}>
       <Text>Hello Button</Text>
     </Button>
   ))
-  .add('with some emoji', () => (
-    <Button onPress={action('clicked-emoji')}>
+  .add("with some emoji", () => (
+    <Button onPress={action("clicked-emoji")}>
       <Text>😀 😎 👍 💯</Text>
     </Button>
   ));
